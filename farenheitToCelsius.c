9be0b7791e82eb4c90afc34e5bddf6dfa19411e4
Sean Reed <seanreed1111@gmail.com>
2014-06-03 14:31:02 -0400

    completes farenheit to celsius asssignment

--- farenheitToCelsius.c
+++ farenheitToCelsius.c
@@ -12,10 +12,29 @@
 double fToC(double f); // function to Convert farenheit to Celsius Temp
 int main(int argc, const char * argv[])
 {
+    int farenheit;
+    int const MINTEMP = 0;
+    int const MAXTEMP = 212;
+
+    printf("Farenheit Temp, Celsius Temp");
     
-    for (int farenheit =-40; farenheit <=212; farenheit++) {
+// Print table of Farenheit to Celsius from -40F to 100F
+    for (farenheit =-40; farenheit <=212; farenheit++) {
         printf("Farenheit temp is %d and Celsius temp is %3.1f\n",farenheit,fToC((double)farenheit));
     }
+    
+// Print table of Farenheit to Celsius from 100F to 40F
+    for (farenheit =100; farenheit <=40; farenheit--) {
+        printf("Farenheit temp is %d and Celsius temp is %3.1f\n",farenheit,fToC((double)farenheit));
+    }
+    
+// Print table of Farenheit to Celsius from MINTEMP to MAXTEMP
+    for (farenheit =MINTEMP; farenheit <=MAXTEMP; farenheit++) {
+        printf("Farenheit temp is %d and Celsius temp is %3.1f\n",farenheit,fToC((double)farenheit));
+    }
+    
+    
+
     return 0;
 }
 
